2bc776d4c55ff4a8738e8fcf3a25bb136ca335af
Ussama Naal <usa.naal@gmail.com>
2019-05-21 19:33:10 -0500

    Modify robot initial postion

--- shell_scripts/test_slam.sh
+++ shell_scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/world/u-world.world" & 
+xterm -e ' ROBOT_INITIAL_POSE="-x 3 -y -4" roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/world/u-world.world' & 
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
